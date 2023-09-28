2baec2d680b6f9be8b8a8d714d9433a71f7b61fe
salma <122889996+salmaaSaamir@users.noreply.github.com>
2023-09-28 19:34:10 +0300

    Add files via upload

---
+++ iti/wwwroot/Controllers/AccountController.cs
@@ -0,0 +1,119 @@
+﻿using iti.Models;
+using iti.ViewModelClasses;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace iti.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<ApplicationUser> userManager;
+        private readonly SignInManager<ApplicationUser> signinManager;
+        public AccountController(UserManager<ApplicationUser> _userManager,SignInManager<ApplicationUser> _signinmnger)
+        {
+            userManager = _userManager;
+            signinManager = _signinmnger;
+        }
+        public IActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(RegisterUserViewModel newRUVM)
+        {
+            if (ModelState.IsValid)
+            {
+                ApplicationUser userModel = new ApplicationUser();
+                userModel.UserName = newRUVM.Name;
+                userModel.PasswordHash = newRUVM.Password;
+                userModel.UserId = newRUVM.Code;
+                IdentityResult result = await userManager.CreateAsync(userModel,newRUVM.Password);
+                if (result.Succeeded)
+                {
+                    await userManager.AddToRoleAsync(userModel,"student");
+                    await signinManager.SignInAsync(userModel, false);
+                    return RedirectToAction("Login");
+                }
+                else
+                {
+                    foreach (var item in result.Errors )
+                    {
+                        ModelState.AddModelError("" , item.Description);
+                    }
+                }
+            }
+            return View(newRUVM);
+        }
+        [Authorize(Roles ="admin")]
+        public IActionResult AddAdmin()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Authorize(Roles = "admin")]
+        public async Task<IActionResult> AddAdmin(RegisterUserViewModel newRUVM)
+        {
+            if (ModelState.IsValid)
+            {
+                ApplicationUser userModel = new ApplicationUser();
+                userModel.UserName = newRUVM.Name;
+                userModel.PasswordHash = newRUVM.Password;
+                userModel.UserId = newRUVM.Code;
+                IdentityResult result = await userManager.CreateAsync(userModel, newRUVM.Password);
+                if (result.Succeeded)
+                {
+                    await userManager.AddToRoleAsync(userModel, "admin");
+                    await signinManager.SignInAsync(userModel, false);
+                    return RedirectToAction("Login");
+                }
+                else
+                {
+                    foreach (var item in result.Errors)
+                    {
+                        ModelState.AddModelError("", item.Description);
+                    }
+                }
+            }
+            return View(newRUVM);
+        }
+        public async Task<IActionResult> Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(LoginViewModel UserVm)
+        {
+            if (ModelState.IsValid)
+            {
+                //check
+                ApplicationUser userModel = await userManager.FindByNameAsync(UserVm.Name);
+                if (userModel != null)
+                {
+                    bool found = await userManager.CheckPasswordAsync(userModel, UserVm.Password);
+                    if (found)
+                    {
+                        List<Claim> claim = new List<Claim>
+                        {
+                            new Claim("UserId", userModel.UserId.ToString() ?? string.Empty)
+                        };
+                        await signinManager.SignInWithClaimsAsync(userModel,UserVm.remmeberMe,claim);
+                          
+                        return RedirectToAction("Index", "Home");
+                    }
+                }
+                ModelState.AddModelError("", "Username and password invalid");
+            }
+            return View(UserVm);
+        }
+        public async Task<IActionResult> LogOut()
+        {
+            await signinManager.SignOutAsync();
+            return RedirectToAction("Login");
+        }
+    }
+}
---
+++ iti/wwwroot/Controllers/RolesController.cs
@@ -0,0 +1,46 @@
+﻿using iti.ViewModelClasses;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace iti.Controllers
+{
+    [Authorize(Roles = "admin")]
+    public class RolesController : Controller
+    {
+        private readonly RoleManager<IdentityRole> roleMang;
+        public RolesController(RoleManager<IdentityRole> _roleMang) 
+        { 
+            roleMang = _roleMang;
+        }
+        public IActionResult NewRole()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> NewRole(RoleViewModel newRole)
+        {
+            if (ModelState.IsValid)
+            {
+                IdentityRole role = new IdentityRole();
+                role.Name = newRole.RoleName;
+               IdentityResult result =  await roleMang.CreateAsync(role);
+                if (result.Succeeded) 
+                {
+                    return View(new RoleViewModel());
+                }
+                else
+                {
+                    foreach (var item in result.Errors)
+                    {
+                        ModelState.AddModelError("", item.Description);
+
+                    }
+                }
+            }
+           
+            return View(newRole);
+        }
+    }
+}
